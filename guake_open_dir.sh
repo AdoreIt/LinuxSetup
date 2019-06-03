8f43127cf5b663c509996c004bc400ddf6649c8e
AdoreIt <NatalieLytvyniuk@gmail.com>
2019-06-03 15:46:27 +0300

    Guake: updated guake_open_dir

--- guake_open_dir.sh
+++ guake_open_dir.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-
-echo \
-'#!/bin/sh
-guake -e "cd ${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}"' \
->~/.local/share/nautilus/scripts/"Open Dir in Guake"
-
-chmod +x ~/.local/share/nautilus/scripts/"Open Dir in Guake"
-
-nautilus ~/.local/share/nautilus/scripts/
+directory=$(dirname "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}")
+guake -n "$directory"
